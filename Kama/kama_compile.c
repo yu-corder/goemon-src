d94e5fd79ab6de16482d16a0c98dd6c96cf14dd2
yu-corder <yuuya.9602@icloud.com>
2026-04-10 14:47:36 +0900

    Feat: Implement label support and two-pass compilation

--- Kama/kama_compile.c
+++ Kama/kama_compile.c
@@ -18,15 +18,54 @@ typedef enum {
     OP_HALT 
 } OpCode;
 
+typedef struct {
+    char name[32];
+    int address;
+} Label;
+
+Label symbol_table[128];
+int label_count_internal = 0;
+
+int find_label(char *name) {
+    for (int i = 0; i < label_count_internal; i++) {
+        if (strncmp(symbol_table[i].name, name, strlen(symbol_table[i].name)) == 0) {
+            return symbol_table[i].address;
+        }
+    }
+    return -1;
+}
+
+
 int main() {
     FILE *src = fopen("examples/study.goe", "r");
     if (!src) { perror("Goemon's scroll (study.goe) not found"); return 1; }
 
     int bytecode[1024];
+    int current_pc = 0;
     int count = 0;
     char line[256];
 
     while (fgets(line, sizeof(line), src)) {
+        char *colon = strchr(line, ':');
+        if (colon) {
+            *colon = '\0';
+            char *newline = strpbrk(line, "\r\n");
+            if (newline) * newline = '\0';
+            strcpy(symbol_table[label_count_internal].name, line);
+            symbol_table[label_count_internal].address = current_pc;
+            label_count_internal++;
+        } else {
+            if (strncmp(line, "push", 4) == 0) current_pc += 2;
+            else if (strstr(line, "jmp") || strstr(line, "jz")) current_pc += 2;
+            else if (strlen(line) > 1) current_pc += 1;
+        }
+    }
+
+    rewind(src);
+
+    while (fgets(line, sizeof(line), src)) {
+        if (strchr(line, ':')) continue;
+
         if (strncmp(line, "push", 4) == 0) {
             int val = atoi(line + 5);
             bytecode[count++] = OP_PUSH;
@@ -49,10 +88,10 @@ int main() {
             bytecode[count++] = OP_EQ;
         } else if (strstr(line, "jmp")) {
             bytecode[count++] = OP_JMP;
-            bytecode[count++] = atoi(line + 3);
+            bytecode[count++] = find_label(line + 4);
         } else if (strstr(line, "jz")) {
             bytecode[count++] = OP_JZ;
-            bytecode[count++] = atoi(line + 3);
+            bytecode[count++] = find_label(line + 3);
         }
         else if (strstr(line, "print")) {
             bytecode[count++] = OP_PRINT;
