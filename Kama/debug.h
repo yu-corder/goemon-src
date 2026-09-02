46fb2ca5c515b7ed56aa7d4cc46e5ba50ad8158f
yu-corder <yuuya.9602@icloud.com>
2026-09-02 20:45:24 +0900

    Refactor: Separate parser, AST, and debug logic

--- Kama/debug.h
+++ Kama/debug.h
@@ -1,10 +1,14 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 #include "token.h"
+#include "ast.h"
+#include "type.h"
 
 extern const char *token_kind_name[];
 
 const char* token_name(TokenKind kind);
-
 void debug_token(int count);
+const char* type_name(TypeKind kind);
+void print_indent(int depth);
+void debug_ast_node(Node *node, int depth);
 #endif
