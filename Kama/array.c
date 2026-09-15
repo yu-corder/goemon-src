6c0b0634d96ffcef87bdc95aa67dbd8496d47ca1
yu-corder <yuuya.9602@icloud.com>
2026-09-15 23:00:36 +0900

    Refactor: Allocate array elements dynamically

--- Kama/array.c
+++ Kama/array.c
@@ -1,15 +1,17 @@
 #include "array.h"
+#include <stdlib.h>
 
 int array_count = 0;
 Array array_table[128];
 
 int make_array(int size) {
+    int *array;
     int index = array_count;
-    int nums[size];
-    array_table[index].elements = nums;
-    for(int i = 0; i < size; i++) {
-        array_table[index].elements[i] = 5;
-    }
+    array = malloc(sizeof(int) * size);
+
+    array_table[index].length = size;
+    array_table[index].elements = array;
+    
     array_count++;
     return index;
 }
